3b5aee657a750c9e73fbd5fc9c5dcd476b3506e1
Samuel Edwin <esam091@gmail.com>
2016-09-29 00:02:07 +0700

    Update example to support Swift 2.3

--- Example/Source/Extensions/UIImage+Color.swift
+++ Example/Source/Extensions/UIImage+Color.swift
@@ -30,11 +30,11 @@ internal extension UIImage {
         let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
         UIGraphicsBeginImageContext(rect.size)
         let context = UIGraphicsGetCurrentContext()
-        CGContextSetFillColorWithColor(context, color.CGColor)
-        CGContextFillRect(context, rect)
+        CGContextSetFillColorWithColor(context!, color.CGColor)
+        CGContextFillRect(context!, rect)
         let image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
-        return image
+        return image!
     }
     
 }
